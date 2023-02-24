6611be94089d04f857f4d1ba01012185e40db61f
James <james@yieldstudio.fr>
2023-02-24 18:55:14 +0100

    disallow curly and configure padding-line-between-statements

--- shared.js
+++ shared.js
@@ -3,13 +3,28 @@ module.exports = {
   rules: {
     radix: 'off',
     'max-len': 'off',
+    curly: ['error', 'all'],
+
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: '*', next: ['return', 'continue', 'default', 'throw'] },
+      {
+        blankLine: 'always',
+        prev: ['multiline-block-like', 'multiline-const', 'multiline-let', 'multiline-var', 'multiline-expression'],
+        next: ['*'],
+      },
+      { blankLine: 'always', prev: ['import'], next: ['*'] },
+      { blankLine: 'any', prev: ['import'], next: ['import'] },
+      { blankLine: 'always', prev: ['export'], next: ['*'] },
+      { blankLine: 'any', prev: ['export'], next: ['export'] },
+      { blankLine: 'always', prev: ['directive'], next: ['*'] },
+    ],
 
     '@typescript-eslint/no-explicit-any': 'warn',
     '@typescript-eslint/consistent-type-imports': ['error', { fixStyle: 'separate-type-imports' }],
     '@typescript-eslint/no-floating-promises': 'off',
     '@typescript-eslint/array-type': ['error', { default: 'array-simple' }],
 
-    'import/prefer-default-export': 'warn',
     'import/order': [
       'error',
       {
