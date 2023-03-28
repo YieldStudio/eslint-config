0bbf624562035f048785bd779fe092ff932a807f
Julien Sanchez-Porro <sanchezporro.julien@outlook.com>
2023-03-28 16:18:00 +0200

    feat(react-native): eslint config

---
+++ react-native.js
@@ -0,0 +1,44 @@
+module.exports = {
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'plugin:react-native/all',
+    '@react-native-community',
+    require.resolve('./shared.js'),
+  ],
+  plugins: ['unicorn', 'react', 'react-hooks', 'react-native'],
+  rules: {
+    'unicorn/filename-case': [
+      'error',
+      {
+        case: 'kebabCase',
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/require-default-props': 'off',
+    'react/jsx-uses-react': 'off',
+    'react/jsx-key': 'error',
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'function-declaration',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-native/no-unused-styles': 'error',
+    'react-native/split-platform-components': 'error',
+    'react-native/no-inline-styles': 'off',
+    'react-native/no-color-literals': 'error',
+    'react-native/no-single-element-style-arrays': 'error',
+    'react-native/no-raw-text': [
+      'warn',
+      {
+        skip: ['Animated.Text', 'Text', 'MotiText', 'Button'],
+      },
+    ],
+  },
+};
